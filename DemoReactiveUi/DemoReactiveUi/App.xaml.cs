50f66355334e7dfc592485846eded306352f54b3
Abdoulaye Dong <abdoulayedong@gmail.com>
2021-05-07 00:15:04 +0100

    First ViewModel using ReactiveUI
    Adds view and viewmodel for the color managing and use RaiseAndSetIfChanged and RaisePropertyChanged from ReactiveUi package.

--- DemoReactiveUi/DemoReactiveUi/App.xaml.cs
+++ DemoReactiveUi/DemoReactiveUi/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoReactiveUi.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace DemoReactiveUi
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new ColorsDemoPage();
         }
 
         protected override void OnStart()
