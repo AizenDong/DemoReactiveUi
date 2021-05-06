25efda3c665db90c5048d9ae690de2b99a9f3b0e
Abdoulaye Dong <abdoulayedong@gmail.com>
2021-05-06 23:27:54 +0100

    DataBinding, Converters, INotifyPropertyChanged
    We add a viewModel, implement INotifyPropertyChanged and create a ColorConverter based on property inside vm.

---
+++ DemoReactiveUi/DemoReactiveUi/Converters/StringToColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DemoReactiveUi.Converters
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null)
+            {
+                var text = value.ToString().ToLower();
+
+                if(text == "blue")
+                {
+                    return Color.Blue;
+                } else if(text == "red")
+                {
+                    return Color.Red;
+                }
+
+                return Color.Green;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ DemoReactiveUi/DemoReactiveUi/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DemoReactiveUi
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _userName;
+
+        public string UserName  
+        {
+            get { return _userName; }
+            set 
+            {
+                _userName = value; 
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(UserName)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
